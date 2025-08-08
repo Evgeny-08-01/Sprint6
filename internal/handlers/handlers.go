2ef16eeb7b5c29696ded4189dc580258d5c621d7
Воробьев Евгений <vorobiev_ev@mail.ru>
2025-08-08 19:49:22 +0300

    3я попытка 0808_19ч49м

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -13,13 +13,14 @@ import (
 	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
 )
 
+ var Logger *log.Logger
 /////////////////////////////////////////////////////////////////////////////////////////////////
 //Реализация хэндлера по запросу "/"/////
 func Handler1(res http.ResponseWriter, req *http.Request) {
 //открыть файл 
 data, err := os.ReadFile("../index.html")
     if err != nil {
-        log.Fatal(err)
+        Logger.Fatal(err)
     }
 
 //возвращается строка в res.Write([]byte(s))
@@ -29,15 +30,16 @@ res.Write([]byte(data))
 // Реализация хэндлера по запросу "/upload"
 func Handler2(res http.ResponseWriter, req *http.Request) {
 // Парсинг формы////
- //    req.ParseMultipartForm(10<<20)   
        if err := req.ParseForm(); err != nil {
-        http.Error(res, /*err.Error()*/"Ошибка при парсинге формы", http.StatusInternalServerError)
+        http.Error(res,"Ошибка при парсинге формы", http.StatusInternalServerError)
+        Logger.Fatal(err)
         return
     }
 // Получение файла из формы////////////////////////////////////////////////////////////////////////////
     file,header, err := req.FormFile("myFile") // "myFile" - название поля файла в форме
     if err != nil { 
         http.Error(res, "Не удалось получить файл", http.StatusInternalServerError)
+        Logger.Fatal(err)
         return
     }
     defer file.Close() //Закрываем файл/////
@@ -45,31 +47,33 @@ func Handler2(res http.ResponseWriter, req *http.Request) {
     data, err := io.ReadAll(file)
     if err != nil {
         http.Error(res, "Ошибка при чтении файла", http.StatusInternalServerError)
+        Logger.Fatal(err)
         return
     }
 //  конвертация строки//////////////////
    convString:=service.Texttomorse_and_revers(string(data))
 
 // Создаем новый файл///////////////////
-	//urlString := req.URL.String()
     now := time.Now()
     fmt.Println(now)
     currentTime1 := now.String()[:11]
     currentTime2 := strings.ReplaceAll(now.String()[11:37], ":", "-", )
-    currentTimeFileExtantion := filepath.Ext(header.Filename)
-    file2, err := os.Create(currentTime1+currentTime2+currentTimeFileExtantion)       
+    fileExtantion := filepath.Ext(header.Filename)
+    file2, err := os.Create(currentTime1+currentTime2+fileExtantion)       
     if err != nil {
         http.Error(res, "Ошибка при создании файла", http.StatusInternalServerError)
+        Logger.Fatal(err)
     }
 //Записываем результат конвертации строки в файл file2////
     _, err = file2.WriteString(convString)
     if err != nil {
+         http.Error(res, "Ошибка при записи результата конвертации строки в файл", http.StatusInternalServerError)
         log.Fatal(err)
     }
 defer file2.Close()
     _,err=res.Write([]byte(convString))
     if err !=   nil {
         http.Error(res, "Ошибка при передаче файла", http.StatusInternalServerError)
+        Logger.Fatal(err)
     }
-
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,11 +1,8 @@
 package service
 
 import (
-//	"fmt"
-
 	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
 )
-
 func Texttomorse_and_revers(str string) string {
 	s1 := morse.ToText(str)
 	s2 := morse.ToMorse(s1)
